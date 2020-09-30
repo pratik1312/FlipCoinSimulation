8f30acf8c7a8a62dc63fbd63dbbcbd11547b2bd1
pratik1312 <patilpratik194@gmail.com>
2020-09-30 19:23:57 +0530

    Added:file decides the winner and by how much

--- flipcoinSimulator.sh
+++ flipcoinSimulator.sh
@@ -7,17 +7,27 @@ do
 x=$(($(($RANDOM%2)) + 1))
 if [ $x -eq 1 ]
 then
-        ((headcount++))
+	((headcount++))
 else
-        ((tailcounter++))
+	((tailcounter++))
 fi
 done
 if [ $headcount -eq $MAXCOUNT ]
 then
-        echo "Head Wins"
-        echo "Head winning time : "$headcount
+	echo "Head Wins"
+	echo "Head winning time : "$headcount
+	echo "Tail winning time : "$tailcounter
+	diff=$(($headcount-$tailcounter))
+	echo "Head wins by difference: "$diff
 else
-        echo "Tail wins"
-        echo "Tail winning time : "$tailcounter
-fi
+	echo "Tail wins"
+	echo "Tail winning time : "$tailcounter
+	echo "Head winning time : "$headcount
+	diff=$(($tailcounter-$headcount))
+        echo "Tail wins by difference: "$diff
 
+fi
+if [ $headcount -eq $tailcounter ]
+then
+	echo "Its a tie"
+fi
