ceef29c814e55c6de8ec63d78f9280465bf2a3af
pratik1312 <patilpratik194@gmail.com>
2020-09-30 09:08:54 +0530

    Added:Flip the coin multiple times and shows the results

--- flipcoinSimulator.sh
+++ flipcoinSimulator.sh
@@ -1,9 +1,23 @@
 ##!/bin/bash -x
-x=$((RANDOM%2))
-echo $x
-if [ $x -eq 0 ]
+MAXCOUNT=21
+headcount=0
+tailcounter=0
+while [[ $headcount -ne $MAXCOUNT && $tailcounter -ne $MAXCOUNT ]]
+do
+x=$(($(($RANDOM%2)) + 1))
+if [ $x -eq 1 ]
 then
-	echo "Head"
+        ((headcount++))
 else
-	echo "Tails"
+        ((tailcounter++))
 fi
+done
+if [ $headcount -eq $MAXCOUNT ]
+then
+        echo "Head Wins"
+        echo "Head winning time : "$headcount
+else
+        echo "Tail wins"
+        echo "Tail winning time : "$tailcounter
+fi
+
